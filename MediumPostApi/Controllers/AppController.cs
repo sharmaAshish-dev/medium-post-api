404a282b85f30e3546a2a77c1cbf975d90cea9f1
Ashish <Ashishking2014@gmail.com>
2024-02-11 12:59:52 +0530

    Add project files.

---
+++ MediumPostApi/Controllers/AppController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace MediumPostApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppController : ControllerBase
+    {
+        [HttpGet(Name = "TestFn")]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
